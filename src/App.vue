db3e04a77e32f5d34d841059233d6193b5a892a7
Mohdan <mohdan.barzankaev@gmail.com>
2021-06-13 01:30:45 +0300

    store/state

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
+<the-navbar></the-navbar>
   <div class="container with-nav">
     <div class="card">
       <h1>Про Vuex</h1>
-      <h2>Счетчик 0</h2>
+      <h2>Счетчик {{ $store.state.counter }}</h2>
+      <button class="btn" @click="increments">+</button>
     </div>
   </div>
 </template>
 
 <script>
-import TheNavbar from './TheNavbar'
+import TheNavbar from './components/TheNavbar'
 export default {
+  methods: {
+    increments() {
+      this.$store.state.counter++
+    }
+  },
   components: {TheNavbar}
 }
 </script>
