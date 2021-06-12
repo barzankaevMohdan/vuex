715f7023d7b8a4f13b85c84b6097827ba4ced777
Mohdan <mohdan.barzankaev@gmail.com>
2021-06-13 00:42:50 +0300

    vuex

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container with-nav">
+    <div class="card">
+      <h1>Про Vuex</h1>
+      <h2>Счетчик 0</h2>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheNavbar from './TheNavbar'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {TheNavbar}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
