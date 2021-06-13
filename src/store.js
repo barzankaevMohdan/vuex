8ebb10251640eb563be595d2ec76a36bc8d7e09b
Mohdan <mohdan.barzankaev@gmail.com>
2021-06-13 13:20:47 +0300

    getters

--- src/store.js
+++ src/store.js
@@ -10,5 +10,16 @@ export default createStore({
         add(state, payLoad) {
             state.counter += payLoad.value
         }
+    },
+    getters: {
+        counter(state) {
+            if(state.counter > 50) {
+                return 0
+            }
+            return state.counter
+        },
+        doubleCounter(_, getters) {
+            return getters.counter * 2
+        }
     }
 })
